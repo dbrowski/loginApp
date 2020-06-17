554fab50523a5d56460c0ec3f326889ba5e7f084
Anthony <akdflyinvee73@gmail.com>
2020-06-17 10:06:33 -0500

    sample app code

---
+++ build/precache-manifest.6285c1df52c0021233492f001dfb59ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c596e3c623753368f53901313ca273",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62d743947c8eda9657de",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "b5a22102620bfa0d09e0",
+    "url": "/static/js/2.786f722f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.786f722f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d743947c8eda9657de",
+    "url": "/static/js/main.0711e5fd.chunk.js"
+  },
+  {
+    "revision": "60cf10ba1b3130053612",
+    "url": "/static/js/runtime-main.9be5d789.js"
+  }
+]);
---
+++ src/config.js
@@ -0,0 +1,19 @@
+export default {
+  // Application authorization details. For more information check "Getting Started" in README.md
+  authDetails: {
+    environmentId: "fa669f8d-f31c-4b71-93b7-ca1906954c6f",
+    responseType: "token id_token",
+    clientId: "2f9b6c22-1637-42c5-9ac8-7d3dd153f77b",
+    clientSecret: "",
+    grantType: "implicit",
+    redirectUri: "https://loginApp.surge.sh",
+    logoutRedirectUri: "https://loginApp.surge.sh",
+    scope: "profile address email phone",
+    prompt: "login",
+    tokenEndpointAuthMethod: "none",
+    maxAge: 3600,
+  },
+
+  API_URI: "https://api.pingone.com/v1",
+  AUTH_URI: "https://auth.pingone.com",
+};
